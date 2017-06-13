c8003f36d50390de1b1335e98bb4ed46e09d1358
Slanj <slanjr@gmail.com>
2017-06-13 15:24:07 +0000

    simle json responce 3

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,17 +1,17 @@
 class WebhookController < ApplicationController
     # GET /welcome
-  def index
-    client = ApiAiRuby::Client.new(
-    :client_access_token => 'cf15b53c62c74255af0f5e7bba8fdaa8'
-    )
-    @response = client.text_request 'hello!' 
+  def index 
+  
+   
     
       
     # Term.create(name: "Co-Marketing", address: "несколько компаний (обычно две) объединяют свои усилия для создания нового уникального продукта, используя свои собственные наработки.", reference: "https://kaplunoff.com/blog/marketing-i-prodazhi/237-marketing-words", synonyms: "Co-Marketing, comarketing, co-marketing")
     #@term = Term.find(3)
     respond_to do |format|
     format.html # show.html.erb
-    format.json { render json: {speech: "Hello",
+    format.json { 
+        response.headers['Content-type'] = 'application/json'
+        render json: {speech: "Hello",
         displayText: "Hello",
         data: "",
         contextout: [],
