f9b513d4e3a7dab9c225f169d50d44f5688e538e
Slanj <slanjr@gmail.com>
2017-06-13 17:29:43 +0000

    disable csrf 2

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,8 +1,8 @@
 class WebhookController < ApplicationController
     # GET /welcome
+ skip_before_action :verify_authenticity_token, only: [:index]    
+    
   def index 
-  
-   
     
       
     # Term.create(name: "Co-Marketing", address: "несколько компаний (обычно две) объединяют свои усилия для создания нового уникального продукта, используя свои собственные наработки.", reference: "https://kaplunoff.com/blog/marketing-i-prodazhi/237-marketing-words", synonyms: "Co-Marketing, comarketing, co-marketing")
@@ -10,7 +10,6 @@ class WebhookController < ApplicationController
     respond_to do |format|
     format.html # show.html.erb
     format.json { 
-        protect_from_forgery with: :null_session
         response.headers['Content-type'] = 'application/json'
         render json: {speech: "Hello",
         displayText: "Hello",
