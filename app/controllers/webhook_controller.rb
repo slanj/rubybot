3ea17ed8d7b70568da4fd48dde84c5aff1cd8739
Slanj <slanjr@gmail.com>
2017-06-13 14:44:44 +0000

    Tesh webhook with ruby api

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,11 +1,17 @@
 class WebhookController < ApplicationController
     # GET /welcome
   def index
- # Term.create(name: "Co-Marketing", address: "несколько компаний (обычно две) объединяют свои усилия для создания нового уникального продукта, используя свои собственные наработки.", reference: "https://kaplunoff.com/blog/marketing-i-prodazhi/237-marketing-words", synonyms: "Co-Marketing, comarketing, co-marketing")
-    @term = Term.find(3)
+    client = ApiAiRuby::Client.new(
+    :client_access_token => 'cf15b53c62c74255af0f5e7bba8fdaa8'
+    )
+    @response = client.text_request 'hello!' 
+    
+      
+    # Term.create(name: "Co-Marketing", address: "несколько компаний (обычно две) объединяют свои усилия для создания нового уникального продукта, используя свои собственные наработки.", reference: "https://kaplunoff.com/blog/marketing-i-prodazhi/237-marketing-words", synonyms: "Co-Marketing, comarketing, co-marketing")
+    #@term = Term.find(3)
     respond_to do |format|
     format.html # show.html.erb
-    format.json { render json: @term }
+    format.json { render json: @response }
     end
   end
     
